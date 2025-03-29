07eeca8f0793554c03aeb4b4c34895a8107c80e0
SmartConTractor <login.blockchain888@gmail.com>
2025-03-29 17:15:39 +0100

    Add comments and a Read Me file.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Empty Tab Sheet Generator
+
+## This python code automatically generates empty guitar tab sheets with dynamic sizing.
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fpdf import FPDF
 
+# Generate PDF
 pdf = FPDF(orientation="P", unit="mm", format="A4") # P for portrait, L for landscape
 pdf.set_auto_page_break(auto=False, margin=0) # pages should not be broken automatically
 
+# Create a blank page
 pdf.add_page()
 
+# Lines and spacing
 pdf.line(20, 28, 190, 28)
 pdf.line(20, 32, 190, 32)
 pdf.line(20, 36, 190, 36)
@@ -12,4 +15,5 @@ pdf.line(20, 40, 190, 40)
 pdf.line(20, 44, 190, 44)
 pdf.line(20, 48, 190, 48)
 
+# Give output
 pdf.output("output/empty-tab-sheet.pdf")
