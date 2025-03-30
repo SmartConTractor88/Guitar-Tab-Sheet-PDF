07e5da9445b65f63b21f528a5bb3c0b83c77534f
SmartConTractor <login.blockchain888@gmail.com>
2025-03-30 10:22:30 +0200

    Add another For loop for vertical side-lines.

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ pdf.add_page()
 lines_per_row = 6
 line_spacing = 3.5 # pixels between lines
 rows_per_sheet = 8
-row_spacing = 8
-from_top = 38
+row_spacing = 10
+from_top = 40
 
+# Horizontal lines
 pdf.line(40, 20, 170, 20)
 
 for row in range(rows_per_sheet):
@@ -24,6 +25,13 @@ for row in range(rows_per_sheet):
 
     from_top += row_spacing # create space between rows
 
+# Vertical side-lindes
+from_top = 40
+for row in range(rows_per_sheet):
+    pdf.line(20, from_top, 20, from_top+line_spacing*(lines_per_row-1))
+    pdf.line(190, from_top, 190, from_top+line_spacing*(lines_per_row-1))
+
+    from_top += line_spacing * lines_per_row + row_spacing
 
 # Give output
 pdf.output("output/empty-tab-sheet.pdf")
